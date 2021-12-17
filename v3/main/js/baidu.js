933e9362c667669d199f8689d068301e997bb124
dev <dev@qiyutech.tech>
2021-12-17 22:42:13 +0800

    add v3 static files

---
+++ v3/main/js/baidu.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || [];
+(function () {
+    var hm = document.createElement("script");
+    hm.src = "https://hm.baidu.com/hm.js?a9740712074ef5362f9ca58c04666d28";
+    var s = document.getElementsByTagName("script")[0];
+    s.parentNode.insertBefore(hm, s);
+})();
